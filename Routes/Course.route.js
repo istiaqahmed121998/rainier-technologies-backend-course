7d28db9022bd04d146aecd6a421f359a5deb3d65
istiaq <istiaqahmed121998@gmail.com>
2023-12-21 20:12:11 +0600

    update method added & fixes bugs

--- Routes/Course.route.js
+++ Routes/Course.route.js
@@ -5,10 +5,10 @@ const CourseService=require("../Services/Course.Service")
 router.post('/',CourseService.addCourse)
 
 router.get('/',CourseService.getCourses)
-router.get('/:id',CourseService.getCourseById)
-// router.get('/course',CourseService.getCourseBySearch)
 
-// router.post('/refresh-token',UserService.refreshToken)
+router.get('/:id',CourseService.getCourseById)
 
 router.delete('/:id/delete',CourseService.deleteById)
+
+router.patch("/:id/update",CourseService.updateById)
 module.exports=router;
